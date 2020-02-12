f500191820231727955b9dee72bd98dc3d53b7f7
iaihabjb <ihab@ihab-baghdadi.com>
2020-02-11 23:30:33 -0500

    'update'

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <meta charset="utf-8"><div class="top-container">
+     <img class="top-cloud" src="img/cloud.png" alt="cloud-img">
+     <h1>I'm Ihab.</h1>
+     <p id="subtitle">a programmer.</p>
+     <img class="bottom-cloud" src="img/cloud.png" alt="cloud-img">
+     <img class="mountain" src="img/mountain.png" alt="mountain-img">
+   </div>
+<div class="middle-container">
+  <div class="profile">
+    <img src="img/profile.jpg" alt="Ihab Baghdadi Profile Picture" style="height: 150px; margin-top: 50px;">
+    <h2>Hello.</h2>
+    <p class="intro">Diligent and tech-savvy graduate with strong coding and web development skills. Problem solver with ability to collaborate with others to come up with solutions, address performance concerns, and debug issues. Strong time management skills, able to meet deadlines, and sees all projects through to completion. Coachable individual, who wants to grow professionally, continue to learn, and be a strong contributor.</p>
+  </div>
+  <hr>
+  <!-- Skills -->
+  <Skills></Skills>
+  <hr>
+  <div class="contact-me">
+    <h2>Get In Touch</h2>
+    <h3>Got a question for me? I'm all ears!</h3>
+    <p class="contact-message">Send me an email and I will get back to you as soon as possible!</p>
+    <a class="btn" href="ihabbaghdadi@live.com">CONTACT ME</a>
+  </div>
+</div>
+
+
+<div class="bottom-container">
+  <a class="footer-link" href="https://www.linkedin.com/in/ihab-baghdadi-web-developer/">LinkedIn</a>
+  <a class="footer-link" href="https://ijb87.github.io/My-Website/">Website</a>
+  <p class="copyright">© 2019 Ihab Baghdadi.</p>
+</div>
+</div>
+</template>
+
+<script>
+
+import Skills from './components/Skills'
+
+export default {
+  name: 'App',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="skills">
+    <h2>My Skills.</h2>
+    <div class="skill-row" v-for="(obj, key) in skillsJSON" :key="key">
+      <h3>~ {{ obj.subtitle }}</h3>
+      <p>{{ obj.paragraph }}</p>
+    </div>
+    <!-- <div class="skill-row">
+      <img class="skill-image" src="img/speech.png" alt="watch image">
+      <h3>Strong Communicator</h3>
+      <p>I've gotten certifications on Lynda.com (LinkedIn Learning) regarding writing and verbal communication. I learned a lot of ways to communicate effectively with others, including being assertive.</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      skillsJSON: [
+        {
+          subtitle: 'Coding',
+          paragraph: 'I learned basic and intermediate HTML, CSS3 and JavaScript in university. I have a certificate on Responsive Web Design from FreeCodeCamp.org and one on Web Programming Foundations from Lynda.com (LinkedIn Learning). I also ranked in the top 25% of those who took the TestDome certification test on HTML, CSS and JavaScript.'
+        },
+        {
+          subtitle: 'Strong Communicator',
+          paragraph: "I've gotten certifications on Lynda.com (LinkedIn Learning) regarding writing and verbal communication. I learned a lot of ways to communicate effectively with others, including being assertive."
+        }
+      ]
+    })
+  }
+</script>
